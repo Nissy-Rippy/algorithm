eedc36342c2797b4cd2cff075c76ca8f5618f76f
Yoshiteru Toyoda <6089more@gmail.com>
2021-07-27 07:15:50 +0000

    [finish] regexp practice

--- regular.rb
+++ regular.rb
@@ -51,16 +51,49 @@ TEXT
 text.scan(/\w+(?=:bass)/)
 # => ["Paul", "John"]
 
-text = <<-TEXT
-John:guitar, George:guitar, Paul:bass, Ringo:drum
-Freddie:vocal, Brian:guitar, John:bass, Roger:drum
-TEXT
-text.scan
 
-東京都
-千葉県
-神奈川県
-埼玉都
 
-(?<!東京)都
+text = "私の誕生日は199年5月45日です"
+m = /(\d+)年(\d+)月(\d+)日/.match(text)
 
+p m
+m[0]
+m[1]
+m[2]
+m[3]
+m[1..3]
+m[-1]
+m[-1,2]
+
+
+text = "私の生まれた年は1229年3月34日です正規表現で引き抜いてください"
+m = text.match(/(?<year>\d+)年(?<month>\d+)月(?<day>\d+)日/)
+puts m
+
+puts m[:year]
+puts m[:month]
+puts m[:day]
+
+
+result = "123 345 564 323".scan(/\d+/)
+puts result
+
+text = "123-4567 123-456-754 34-2332"
+puts text.slice(/(\d{3})-(\d{4})/,1)
+
+
+text = "123-3456-23.567"
+
+puts text.gsub(/-|\./,"@")
+
+#egexp.new("\d{3}-\d{4}")
+#pattern = /\d{3}-\d{4}/
+
+
+text = "私の誕生日は1999年23月4日ですおめでと０"
+text =~ /(\d+)年(\d+)月(\d+)日/
+p Regexp.last_match(0)
+
+p Regexp.last_match(1)
+p Regexp.last_match(2)
+p Regexp.last_match(-1)
