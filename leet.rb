8a2a5ec745be5fe91beec10d7c227900fa495b33
Yoshiteru Toyoda <6089more@gmail.com>
2021-08-11 14:05:55 +0000

    leet

--- leet.rb
+++ leet.rb
@@ -22,3 +22,27 @@ strings.each do | chara |
 
  output = convert.join
  puts output
+ 
+ input_line = gets
+ strings = input_line.to_s.split("")
+ convert = []
+ strings.each do |chara|
+   case chara
+   
+   when "A" then
+     convert.push("4")
+     when "B" then
+       convert.push("3")
+       when "c" then
+         convert.push("6")
+         when "d" then
+           convert.push("2")
+         else
+           convert.push(chara)
+         end
+       end
+       
+       out_putline = convert.join
+       puts output
+       
+       
